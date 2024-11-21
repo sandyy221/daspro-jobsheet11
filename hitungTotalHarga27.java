cacc5e7a55f2e1c7d975f8998e8cd4bd59049d24
sandyy221 <kurniawansandy808.com>
2024-11-21 12:18:46 +0700

    modifikasi pertanyaan percobaan3

--- hitungTotalHarga27.java
+++ hitungTotalHarga27.java
@@ -5,6 +5,17 @@ public class hitungTotalHarga27 {
     public static int hitungTotalHarga27 (int pilihanMenu, int banyakItem, String kodePromo) {
         int [] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
         int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+
+        if ("DISKON50".equals(kodePromo)) {
+            System.out.println("Berikan diskon 50%");
+            hargaTotal = hargaTotal - (hargaTotal / 2);
+        } else if ("DISKON30".equals(kodePromo)) {
+            System.out.println("Berikan diskon 30%");
+            hargaTotal = hargaTotal - (hargaTotal * 30 / 100); 
+        } else {
+            System.out.println("Kode promo invalid.");
+        }
+
         return hargaTotal;
 
     }
@@ -14,7 +25,9 @@ public class hitungTotalHarga27 {
        int pilihanMenu = sc.nextInt();
        System.out.print("Masukan jumlah item yang ingin dipesan: ");
        int banyakItems = sc.nextInt();
-       int totalHarga = hitungTotalHarga27(pilihanMenu, banyakItems, "DISKON50");
+       System.out.print("Masukan kode promo: ");
+       String kodePromo = sc.next();
+       int totalHarga = hitungTotalHarga27(pilihanMenu, banyakItems, kodePromo);
        System.out.println("Total harga untuk pesanan saya: Rp" + totalHarga);
 
         
