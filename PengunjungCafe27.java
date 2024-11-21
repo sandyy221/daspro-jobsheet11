6ba2864fcd9f1bc87afa3f0aa2a9a69dca89e65b
sandyy221 <kurniawansandy808.com>
2024-11-21 14:32:18 +0700

    modifikasi pertanyaan percobaan4

--- PengunjungCafe27.java
+++ PengunjungCafe27.java
@@ -1,8 +1,8 @@
 public class PengunjungCafe27 {
     public static void daftarPengunjung (String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjungn: ");
-        for (int i=0;i<namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
     }
     public static void main(String[] args) {
