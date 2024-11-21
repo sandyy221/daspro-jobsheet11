a66d623fe789e1d9e3a557550cd6bd4cdcef5c8a
sandyy221 <kurniawansandy808.com>
2024-11-21 07:24:10 +0700

    Percobaan1

---
+++ Kafe27.java
@@ -0,0 +1,18 @@
+public class Kafe27 {
+
+    public static void menu() {
+        System.out.println("==========MENU RESTO KAFE==========");
+        System.out.println("1. Kopi Hitam - Rp 15000");
+        System.out.println("2.Capucino - Rp 20000 ");
+        System.out.println("3. Latte - Rp 22000");
+        System.out.println("4.Teh Tarik - Rp 12000 ");
+        System.out.println("5. Roti Bakar - Rp 10000");
+        System.out.println("6. Mie Goreng - Rp 18000");
+        System.out.println("===================================");
+        System.out.println("Silahkan Pilih Menu Yang Anda Inginkan");
+    }
+
+    public static void main(String[] args) {
+        menu();
+    }
+}
